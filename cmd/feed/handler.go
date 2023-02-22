9d46298597e5e8eddc3e10f3fd0e39e83be7d322
Ephmeral <2669128599@qq.com>
2023-02-22 17:21:59 +0800

    modify feed service

--- cmd/feed/handler.go
+++ cmd/feed/handler.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	client "github.com/Ephmeral/douyin/cmd/feed/service"
+	"github.com/Ephmeral/douyin/dal/pack"
 	feed "github.com/Ephmeral/douyin/kitex_gen/feed"
+	"github.com/Ephmeral/douyin/pkg/errno"
 )
 
 // FeedServiceImpl implements the last service interface defined in the IDL.
@@ -10,6 +13,23 @@ type FeedServiceImpl struct{}
 
 // Feed implements the FeedServiceImpl interface.
 func (s *FeedServiceImpl) Feed(ctx context.Context, req *feed.FeedRequest) (resp *feed.FeedResponse, err error) {
-	// TODO: Your code here...
-	return
+	// 创建一个新的响应结构体
+	resp = new(feed.FeedResponse)
+
+	if req.LatestTime <= 0 { // 如果LatestTime小于等于0，返回参数错误
+		// 用参数错误的信息构建一个BaseResp结构体并存储到响应结构体中
+		resp.BaseResp = pack.BuildFeedBaseResp(errno.ParamErr)
+		return resp, nil
+	}
+
+	// 如果LatestTime大于0，则调用Feed服务中的服务实现处理Feed请求
+	videos, nextTime, err := client.NewFeedService(ctx).Feed(req)
+	if err != nil {
+		resp.BaseResp = pack.BuildFeedBaseResp(err)
+		return resp, nil
+	}
+	resp.BaseResp = pack.BuildFeedBaseResp(errno.Success)
+	resp.VideoList = videos  // 将视频列表设置到响应结构体中
+	resp.NextTime = nextTime // 将视频列表设置到响应结构体中
+	return resp, nil
 }
---
+++ cmd/feed/service/feed.go
@@ -0,0 +1,95 @@
+package service
+
+import (
+	"context"
+	"sync"
+
+	"github.com/Ephmeral/douyin/dal/db"
+	"github.com/Ephmeral/douyin/dal/pack"
+	"github.com/Ephmeral/douyin/kitex_gen/feed"
+	"github.com/Ephmeral/douyin/pkg/constants"
+	"github.com/Ephmeral/douyin/pkg/jwt"
+)
+
+type FeedService struct {
+	ctx context.Context
+}
+
+// NewFeedService 创建新的FeedService
+func NewFeedService(ctx context.Context) *FeedService {
+	return &FeedService{ctx: ctx}
+}
+
+// Feed 用于获取多个视频信息列表
+func (s *FeedService) Feed(req *feed.FeedRequest) ([]*feed.Video, int64, error) {
+	// 初始化 JWT 对象
+	Jwt := jwt.NewJWT([]byte(constants.SecretKey))
+
+	// 检查用户登录状态
+	currentId, _ := Jwt.CheckToken(req.Token)
+
+	// 查询视频信息
+	videoData, err := db.QueryVideoByLatestTime(s.ctx, req.LatestTime)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// 查询视频 ID 和用户 ID
+	videoIds := make([]int64, 0)
+	userIds := make([]int64, 0)
+	for _, video := range videoData {
+		videoIds = append(videoIds, int64(video.ID))
+		userIds = append(userIds, video.UserId)
+	}
+
+	// 查询用户信息
+	users, err := db.QueryUserByIds(s.ctx, userIds)
+	if err != nil {
+		return nil, 0, err
+	}
+	userMap := make(map[int64]*db.UserRaw)
+	for _, user := range users {
+		userMap[int64(user.ID)] = user
+	}
+
+	var favoriteMap map[int64]*db.FavoriteRaw
+	var relationMap map[int64]*db.RelationRaw
+	// 如果用户未登录
+	if currentId == -1 {
+		favoriteMap = nil
+		relationMap = nil
+	} else {
+		var wg sync.WaitGroup
+		wg.Add(2)
+		var favoriteErr, relationErr error
+		//获取点赞信息
+		go func() {
+			defer wg.Done()
+			favoriteMap, err = db.QueryFavoriteByIds(s.ctx, currentId, videoIds)
+			if err != nil {
+				favoriteErr = err
+				return
+			}
+		}()
+		//获取关注信息
+		go func() {
+			defer wg.Done()
+			relationMap, err = db.QueryRelationByIds(s.ctx, currentId, userIds)
+			if err != nil {
+				relationErr = err
+				return
+			}
+		}()
+		wg.Wait()
+		if favoriteErr != nil {
+			return nil, 0, favoriteErr
+		}
+		if relationErr != nil {
+			return nil, 0, relationErr
+		}
+
+	}
+	// 打包视频信息
+	videos, nextTime := pack.VideoInfo(currentId, videoData, userMap, favoriteMap, relationMap)
+	return videos, nextTime, nil
+}
