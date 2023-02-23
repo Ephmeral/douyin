bca02cca5fcf0b69c3c906d7815ed246521ac5f4
Ephmeral <2669128599@qq.com>
2023-02-23 18:45:01 +0800

    modify cache

--- dal/cache/index_map.go
+++ dal/cache/index_map.go
@@ -35,13 +35,19 @@ func NewProxyIndexMap() *ProxyIndexMap {
 }
 
 // 更新点赞状态
-func (i *ProxyIndexMap) UpdateFavorState(userId int64, videoId int64, state bool) {
-	key := fmt.Sprintf("%d_%s", userId, "favoriteVideo")
+func (i *ProxyIndexMap) UpdateFavorState(userId int64, videoId int64, state bool) error {
+	tx := rdb.TxPipeline()
+	keyUser := fmt.Sprintf("%d_%s", userId, "favoriteVideo")
+	keyVideo := fmt.Sprintf("%d_%s", videoId, "isFavoritedBy")
 	if state {
-		rdb.SAdd(ctx, key, videoId)
-		return
+		tx.SAdd(ctx, keyUser, videoId)
+		tx.SAdd(ctx, keyVideo, userId)
+	} else {
+		tx.SRem(ctx, keyUser, videoId)
+		tx.SRem(ctx, keyVideo, userId)
 	}
-	rdb.SRem(ctx, key, videoId)
+	_, err := tx.Exec(ctx)
+	return err
 }
 
 // 得到点赞状态
@@ -49,6 +55,16 @@ func (i *ProxyIndexMap) GetFavorState(userId int64, videoId int64) bool {
 	key := fmt.Sprintf("%d_%s", userId, "favoriteVideo")
 	state := rdb.SIsMember(ctx, key, videoId)
 	return state.Val()
+}
+
+// 获取用户的点赞总数量
+func (i *ProxyIndexMap) GetFavorCount(userId int64) (int64, error) {
+	key := fmt.Sprintf("%d_%s", userId, "favoriteVideo")
+	cnt, err := rdb.SCard(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+	return cnt, nil
 }
 
 // 获取一个用户所有点过赞的视频id,以切片的形式返回
@@ -79,4 +95,14 @@ func (i *ProxyIndexMap) GetFavorVideoIdsBySet(userId int64) (map[int64]struct{},
 		videoIdsSet[videoId] = struct{}{}
 	}
 	return videoIdsSet, nil
+}
+
+// 获取一个视频被点赞的总数量
+func (i *ProxyIndexMap) GetVideoIsFavoritedCount(videoId int64) (int64, error) {
+	key := fmt.Sprintf("%d_%s", videoId, "favoriteVideo")
+	cnt, err := rdb.SCard(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+	return cnt, nil
 }
